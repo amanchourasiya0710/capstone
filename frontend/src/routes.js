9608cfac6011f810f1c485c18f959ebc864d3086
Aman Chourasiya <2019csb1067@iitrpr.ac.in>
2022-05-12 18:01:20 +0530

    doit

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -8,6 +8,7 @@ import Activate from './pages/Activate';
 import DashboardApp from './pages/DashboardApp';
 import Products from './pages/Products';
 import Blog from './pages/Blog';
+import Check from "./pages/check";
 import User from './pages/User';
 import NotFound from './pages/Page404';
 import Home from './pages/Home';
@@ -18,30 +19,31 @@ import LayoutLayout from './pages/LayoutLayout'
 export default function Router() {
   return useRoutes([
     {
-      path: '/',
+      path: "/",
       element: <LogoOnlyLayout />,
       children: [
         // { path: '/', element: <Navigate to="/dashboard/app" /> },
-        { path: '/', element: <LayoutLayout /> },
-        { path: 'login', element: <Login /> },
-        { path: 'register', element: <Register /> },
+        { path: "/", element: <LayoutLayout /> },
+        { path: "login", element: <Login /> },
+        { path: "check", element: <Check /> },
+        { path: "register", element: <Register /> },
         // { path: 'reset-password', element: <ResetPassword /> },
         // { path: 'password/reset/confirm/:uid/:token', element: <ResetPasswordConfirm /> },
-        { path: 'activate/:uid/:token', element: <Activate /> },
-        { path: '404', element: <NotFound /> },
-        { path: '*', element: <Navigate to="/404" /> }
-      ]
+        { path: "activate/:uid/:token", element: <Activate /> },
+        { path: "404", element: <NotFound /> },
+        { path: "*", element: <Navigate to="/404" /> },
+      ],
     },
     {
-        path: '/dashboard',
-        element: <DashboardLayout />,
-        children: [
-            { path: 'app', element: <DashboardApp /> },
-            { path: 'user', element: <User /> },
-            { path: 'products', element: <Products /> },
-            { path: 'blog', element: <Blog /> }
-        ]
+      path: "/dashboard",
+      element: <DashboardLayout />,
+      children: [
+        { path: "app", element: <DashboardApp /> },
+        { path: "user", element: <User /> },
+        { path: "products", element: <Products /> },
+        { path: "blog", element: <Blog /> },
+      ],
     },
-    { path: '*', element: <Navigate to="/404" replace /> }
+    { path: "*", element: <Navigate to="/404" replace /> },
   ]);
 }
